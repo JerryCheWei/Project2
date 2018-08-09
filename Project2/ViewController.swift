f8a891d0fcc4807f6d7c676a53917ea30a8b2ea8
JerryCheWei <jerrychang585@gmail.com>
2018-08-09 12:59:53 +0800

    add new project

---
+++ Project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by chang-che-wei on 2018/8/9.
+//  Copyright © 2018年 chang-che-wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
