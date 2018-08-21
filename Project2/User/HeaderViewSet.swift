a882b76401ee4595aba4009d820c9d7fd325c080
JerryCheWei <jerrychang585@gmail.com>
2018-08-21 22:45:55 +0800

    animate Header修改

---
+++ Project2/User/HeaderViewSet.swift
@@ -0,0 +1,39 @@
+//
+//  HeaderViewSet.swift
+//  Project2
+//
+//  Created by chang-che-wei on 2018/8/21.
+//  Copyright © 2018年 chang-che-wei. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HeaderViewSet {
+
+   static func decrementColorAlpha(offset: CGFloat, view: UIView) {
+        if view.alpha <= 1 {
+            let alphaOffset = (offset/500)/85
+            view.alpha += alphaOffset
+        }
+    }
+   static func decrementArticleAlpha(offset: CGFloat, label: UILabel) {
+        if label.alpha >= 0 {
+            let alphaOffset = max((offset - 65)/85.0, 0)
+            label.alpha = alphaOffset
+        }
+    }
+   static func incrementColorAlpha(offset: CGFloat, view: UIView) {
+        if view.alpha >= 0.6 {
+            let alphaOffset = (offset/200)/85
+            view.alpha -= alphaOffset
+        }
+    }
+   static func incrementArticleAlpha(offset: CGFloat, label: UILabel) {
+        if label.alpha <= 1 {
+            let alphaOffset = max((offset - 65)/85, 0)
+            label.alpha = alphaOffset
+        }
+    }
+
+}
--- Project2/User/UserVC/UserViewController.swift
+++ Project2/User/UserVC/UserViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class UserViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var colorView: UIVisualEffectView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var introductionLabel: UILabel!
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var userTableView: UITableView!
     var headerHeightConstraint: NSLayoutConstraint!
-    let cellSpacingHeight: CGFloat = 15
+    let cellSpacingHeight: CGFloat = 25
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,18 +37,17 @@ class UserViewController: UIViewController, UITableViewDelegate, UITableViewData
     //建立header animate
     func setHeader() {
         headerView.translatesAutoresizingMaskIntoConstraints = false
-        headerHeightConstraint = headerView.heightAnchor.constraint(equalToConstant: 360)
+        headerHeightConstraint = headerView.heightAnchor.constraint(equalToConstant: 470)
         headerHeightConstraint.isActive = true
     }
     func setTableView() {
         userTableView.translatesAutoresizingMaskIntoConstraints = false
         userTableView.delegate = self
         userTableView.dataSource = self
-        userTableView.layer.cornerRadius = 10
     }
     func animateHeader() {
-        self.headerHeightConstraint.constant = 360
-        UIView.animate(withDuration: 0.1, delay: 0.0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+        self.headerHeightConstraint.constant = 470
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
             self.view.layoutIfNeeded()
         }, completion: nil)
     }
@@ -74,11 +76,15 @@ class UserViewController: UIViewController, UITableViewDelegate, UITableViewData
             }
 
             }.resume()
+        cell.layer.shadowOffset = CGSize(width: 5, height: 5)
+        cell.layer.shadowOpacity = 0.7
+        cell.layer.shadowRadius = 5
+        cell.layer.shadowColor = UIColor(red: 44.0/255.0, green: 62.0/255.0, blue: 80.0/255.0, alpha: 1.0).cgColor
 
         cell.userImageView.image = UIImage(named: "iconIdentity36pt" )
-        cell.layer.cornerRadius = 10
         return cell
     }
+
 }
 
 extension UserViewController: UIScrollViewDelegate {
@@ -87,24 +93,30 @@ extension UserViewController: UIScrollViewDelegate {
         print(scrollView.contentOffset.y)
         if scrollView.contentOffset.y < 0 {
             self.headerHeightConstraint.constant += abs(scrollView.contentOffset.y)
-        }
-        else if scrollView.contentOffset.y > 0 && self.headerHeightConstraint.constant >= 65 {
-            self.headerHeightConstraint.constant -= scrollView.contentOffset.y/50
-            if self.headerHeightConstraint.constant < 65 {
-                self.headerHeightConstraint.constant = 65
-            }
 
+            HeaderViewSet.incrementColorAlpha(offset: headerHeightConstraint.constant, view: self.colorView)
+            HeaderViewSet.incrementArticleAlpha(offset: headerHeightConstraint.constant, label: self.introductionLabel)
+        }
+        else if scrollView.contentOffset.y > 0 && self.headerHeightConstraint.constant >= 75 {
+            self.headerHeightConstraint.constant -= scrollView.contentOffset.y/5
+
+            HeaderViewSet.decrementColorAlpha(offset: headerHeightConstraint.constant, view: self.colorView)
+            HeaderViewSet.decrementArticleAlpha(offset: headerHeightConstraint.constant, label: self.introductionLabel)
+            if self.headerHeightConstraint.constant < 75 {
+                self.headerHeightConstraint.constant = 75
+                self.introductionLabel.alpha = 0
+            }
         }
     }
 
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        if self.headerHeightConstraint.constant > 360 {
+        if self.headerHeightConstraint.constant > 470 {
             animateHeader()
         }
     }
 
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        if self.headerHeightConstraint.constant > 360 {
+        if self.headerHeightConstraint.constant > 470 {
             animateHeader()
         }
     }
