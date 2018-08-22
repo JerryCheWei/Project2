bb96c9f0800eb43c15022dda50311a1fc31cd6ff
JerryCheWei <jerrychang585@gmail.com>
2018-08-22 14:14:49 +0800

    signup 錯誤提示

--- Project2/Login/SignUpVC/SignUpViewController.swift
+++ Project2/Login/SignUpVC/SignUpViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
 
@@ -22,7 +23,47 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
+
+    @IBAction func sendButton(_ sender: UIButton) {
+        if userNameTextField.text == "" ||
+            emailTextField.text == "" ||
+            passwordTextField.text == "" ||
+            confirmPasswordTextField.text == "" {
+
+            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else if passwordTextField.text == confirmPasswordTextField.text {
+            Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user, error) in
+
+                if error == nil, user == user {
+                    Auth.auth().signIn(withEmail: self.emailTextField.text!,
+                                       password: self.passwordTextField.text!, completion: nil)
+                    print("success sign up !!!")
+                }
+                if let error = error {
+                    // 註冊錯誤警告
+                    let alert = UIAlertController(title: "Sign Up Failed",
+                                                  message: error.localizedDescription,
+                                                  preferredStyle: .alert)
+
+                    alert.addAction(UIAlertAction(title: "OK", style: .default))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            })
+        }
+        else if passwordTextField.text != confirmPasswordTextField.text {
+            let alertController = UIAlertController(title: "Password Error", message: "Please confirm your password.", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+
+            self.present(alertController, animated: true, completion: nil)
+        }
+
+    }
+
 }
